3446dcee4ccfebf4081b30c46b3531cd699643ab
MrDogeBro <35872177+aedanbricker@users.noreply.github.com>
2021-05-01 18:23:21 -0400

    Add sub command for uninstall as well as modify aliases

--- src/main.rs
+++ src/main.rs
@@ -4,14 +4,20 @@ use structopt::StructOpt;
 
 #[derive(StructOpt)]
 enum Droid {
-    #[structopt(aliases = &["i", "-S"])]
+    #[structopt(aliases = &["-S", "add", "i"])]
     Install { package: String },
+
+    #[structopt(aliases = &["-R", "remove", "rm", "un"])]
+    Uninstall { package: String },
 }
 
 fn main() {
     match Droid::from_args() {
         Droid::Install { package } => {
             commands::install(package);
+        }
+        Droid::Uninstall { package } => {
+            commands::uninstall(package);
         }
     }
 }
