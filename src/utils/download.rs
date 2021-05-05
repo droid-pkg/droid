fc8a6cb42acb1ee6429cbda1f116c26cd483b8ea
MrDogeBro <35872177+aedanbricker@users.noreply.github.com>
2021-05-04 21:15:25 -0400

    Get file size before downloading

--- src/utils/download.rs
+++ src/utils/download.rs
@@ -1,8 +1,21 @@
 use anyhow::Result;
+use reqwest::header;
 use tokio::{fs, io::AsyncWriteExt};
 
 pub async fn download(url: String, output_file_path: String, file_name: String) -> Result<i32> {
     let client = reqwest::Client::new();
+
+    let file_size = match client.head(&url).send().await {
+        Ok(resp) => resp
+            .headers()
+            .get(header::CONTENT_LENGTH)
+            .and_then(|len| len.to_str().ok())
+            .and_then(|len| len.parse().ok())
+            .unwrap_or(0),
+        Err(e) => return Err(anyhow::anyhow!(e)),
+    };
+
+    println!("{}", file_size);
 
     let mut file_download = client.get(url).send().await?;
     let mut output_file = fs::OpenOptions::new()
