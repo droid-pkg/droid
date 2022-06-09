2a409a876ddcc065b26a8a0d39428b170ddeeaa4
MrDogeBro <MrDogeBro@users.noreply.github.com>
2022-06-09 09:28:22 -0400

    Update to use toml instead of yaml

--- src/utils/build.rs
+++ src/utils/build.rs
@@ -2,12 +2,34 @@ use super::chroot;
 use anyhow::Result;
 use git2::Repository;
 
-pub async fn build(build_path: String, client: reqwest::Client) -> Result<i32> {
+use crate::commands;
+use crate::utils;
+
+pub async fn build(
+    build_path: String,
+    client: reqwest::Client,
+    instructions: utils::InstallInstructions,
+) -> Result<i32> {
     chroot::chroot(build_path).await?;
 
+    for pkg in instructions.dist.build.unwrap().depends {
+        let depend_instructions =
+            commands::install::get_instructions(client.to_owned(), pkg).await?;
+        let instructs = utils::InstallInstructions::parse(depend_instructions.data);
+
+        if depend_instructions.official {
+            // commands::install::install_bin(instructions);
+        } else {
+        }
+    }
+
     Repository::clone(
-        format!("https://github.com/{}/{}.git", "MrDogeBro", "quicknav").as_str(),
-        "quicknav",
+        format!(
+            "https://github.com/{}/{}.git",
+            instructions.info.repo_owner, instructions.info.repo_name
+        )
+        .as_str(),
+        instructions.info.repo_name,
     )?;
 
     Ok(0)
